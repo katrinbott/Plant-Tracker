c1bba398572b09bb295cb3416d9a22fb91af612e
Katrin Bott <katrin.bott96@gmail.com>
2026-06-08 17:58:02 +0200

    Add plant controller and watering controller.

--- src/main/java/org/example/plants/controller/PlantController.java
+++ src/main/java/org/example/plants/controller/PlantController.java
@@ -1,4 +1,38 @@
 package org.example.plants.controller;
 
+import jakarta.validation.Valid;
+import org.example.plants.dto.PlantCreateRequest;
+import org.example.plants.model.Plant;
+import org.example.plants.service.PlantService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/plants")
 public class PlantController {
+
+    private final PlantService plantService;
+
+    public PlantController(PlantService plantService) {
+        this.plantService = plantService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Plant createPlant(@RequestBody @Valid PlantCreateRequest request) {
+        return plantService.createPlant(request);
+    }
+
+    @GetMapping
+    public List<Plant> getAllPlants() {
+        return plantService.getAllPlants();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePlant(@PathVariable Long id) {
+        plantService.deletePlant(id);
+    }
 }
--- src/main/java/org/example/plants/controller/WateringController.java
+++ src/main/java/org/example/plants/controller/WateringController.java
@@ -1,4 +1,44 @@
 package org.example.plants.controller;
 
+import jakarta.validation.Valid;
+import org.example.plants.dto.WaterCreateRequest;
+import org.example.plants.model.WateringEvent;
+import org.example.plants.service.PlantService;
+import org.example.plants.service.WateringService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/watering")
 public class WateringController {
+
+    private final WateringService wateringService;
+
+    public WateringController(WateringService wateringService) {
+        this.wateringService = wateringService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public WateringEvent recordWatering(@RequestBody @Valid WaterCreateRequest.WateringRequest wateringRequest) {
+        return wateringService.recordWatering(wateringRequest.plantId(), wateringRequest.amountMl(), wateringRequest.note());
+    }
+
+    @GetMapping
+    public List<WateringEvent> getAllWateringEvents() {
+        return wateringService.getAllWateringEvents();
+    }
+
+    @GetMapping("/plant/{plantId}")
+    public List<WateringEvent> getWateringHistoryForPlant(@PathVariable Long plantId) {
+        return wateringService.getWateringHistoryForPlant(plantId);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteWateringEvent(@PathVariable Long id) {
+        wateringService.deleteWateringEvent(id);
+    }
 }
